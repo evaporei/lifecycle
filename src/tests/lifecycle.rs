0408ba153a75dd9a6f105608874b4fab1361c5cc
Otávio Pace <otaviopp8@gmail.com>
2020-07-12 15:54:42 -0300

    tests: move types and functions to mocks module

---
+++ src/tests/lifecycle.rs
@@ -0,0 +1,20 @@
+use crate::Lifecycle;
+use crate::tests::mocks::{Database, QueryResult};
+
+#[test]
+fn simple_database() {
+    let database = Database::new("localhost", "3000");
+    assert!(database.connection.is_none());
+
+    let database = database.start();
+    assert!(database.connection.is_some());
+
+    let add_user_result = database.add_user("joana", "green");
+    assert_eq!(add_user_result, QueryResult::Insert);
+
+    let get_user_result = database.get_user("joana");
+    assert_eq!(get_user_result, QueryResult::Search);
+
+    let database = database.stop();
+    assert!(database.connection.is_none());
+}
--- src/tests/mocks.rs
+++ src/tests/mocks.rs
@@ -1,9 +1,9 @@
 use crate::Lifecycle;
 
-struct Database<'a> {
+pub struct Database<'a> {
     host: &'a str,
     port: &'a str,
-    connection: Option<Connection>,
+    pub connection: Option<Connection>,
 }
 
 impl Lifecycle for Database<'_> {
@@ -30,20 +30,20 @@ impl Lifecycle for Database<'_> {
 }
 
 impl<'a> Database<'a> {
-    fn new(host: &'a str, port: &'a str) -> Self {
+    pub fn new(host: &'a str, port: &'a str) -> Self {
         Self {
             host,
             port,
             connection: None,
         }
     }
-    fn get_user(&self, username: &str) -> QueryResult {
+    pub fn get_user(&self, username: &str) -> QueryResult {
         match &self.connection {
             Some(conn) => conn.execute_query(&["SELECT * FROM users WHERE username = ?", username]),
             None => QueryResult::Error,
         }
     }
-    fn add_user(&self, username: &str, favorite_color: &str) -> QueryResult {
+    pub fn add_user(&self, username: &str, favorite_color: &str) -> QueryResult {
         match &self.connection {
             Some(conn) => conn.execute_insert(&[
                 "INSERT INTO users (username, favorite_color)",
@@ -55,51 +55,33 @@ impl<'a> Database<'a> {
     }
 }
 
-struct Connection;
+pub struct Connection;
 
-fn connect_to_database(_host: &str, _port: &str) -> Connection {
+pub fn connect_to_database(_host: &str, _port: &str) -> Connection {
     println!("Opening database connection");
 
     Connection
 }
 
 impl Connection {
-    fn execute_query(&self, _args: &[&str]) -> QueryResult {
+    pub fn execute_query(&self, _args: &[&str]) -> QueryResult {
         println!("execute_query");
 
         QueryResult::Search
     }
-    fn execute_insert(&self, _args: &[&str]) -> QueryResult {
+    pub fn execute_insert(&self, _args: &[&str]) -> QueryResult {
         println!("execute_insert");
 
         QueryResult::Insert
     }
-    fn close(&self) {
+    pub fn close(&self) {
         println!("Closing database connection");
     }
 }
 
 #[derive(Debug, PartialEq)]
-enum QueryResult {
+pub enum QueryResult {
     Search,
     Insert,
     Error,
-}
-
-#[test]
-fn simple_database() {
-    let database = Database::new("localhost", "3000");
-    assert!(database.connection.is_none());
-
-    let database = database.start();
-    assert!(database.connection.is_some());
-
-    let add_user_result = database.add_user("joana", "green");
-    assert_eq!(add_user_result, QueryResult::Insert);
-
-    let get_user_result = database.get_user("joana");
-    assert_eq!(get_user_result, QueryResult::Search);
-
-    let database = database.stop();
-    assert!(database.connection.is_none());
 }
