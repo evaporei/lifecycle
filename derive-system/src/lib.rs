ddba1f27deab8d70bce8594e7f1730f9bc872793
Otávio Pace <otaviopp8@gmail.com>
2021-05-27 00:12:13 -0300

    refactor: make derive-system more modularized

--- derive-system/src/lib.rs
+++ derive-system/src/lib.rs
@@ -1,28 +1,16 @@
 use proc_macro::TokenStream;
-use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
+mod structs;
+mod system;
+
 #[proc_macro_derive(System)]
-pub fn derive(input: TokenStream) -> TokenStream {
+pub fn derive_system(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     let type_name = &input.ident;
 
-    let expanded = quote! {
-        impl lifecycle::Lifecycle for #type_name {
-            fn start(self) -> Self {
-                let mut s = self;
-
-                s
-            }
-
-            fn stop(self) -> Self {
-                let mut s = self;
-
-                s
-            }
-        }
-    };
-
-    expanded.into()
+    system::expand(&type_name, &input.data)
+        .unwrap_or_else(|e| e.to_compile_error())
+        .into()
 }
---
+++ derive-system/src/system.rs
@@ -0,0 +1,49 @@
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::{punctuated::Punctuated, token::Comma, Data, DataStruct, Error, Field, Fields, Ident};
+
+pub fn expand(type_name: &Ident, data: &Data) -> Result<TokenStream2, Error> {
+    match data {
+        Data::Struct(ref data_struct) => expand_struct(type_name, data_struct),
+        Data::Enum(ref data_enum) => Err(Error::new(
+            data_enum.enum_token.span,
+            "derive-system does not support derive for enums yet",
+        )),
+        Data::Union(ref data_union) => Err(Error::new(
+            data_union.union_token.span,
+            "derive-system does not support derive for unions",
+        )),
+    }
+}
+
+fn expand_struct(struct_name: &Ident, data_struct: &DataStruct) -> Result<TokenStream2, Error> {
+    match data_struct.fields {
+        Fields::Named(ref fields) => Ok(expand_named_struct(struct_name, &fields.named)),
+        Fields::Unnamed(_) => Err(Error::new(
+            data_struct.struct_token.span,
+            "derive-system does not support derive for unnamed structs yet",
+        )),
+        Fields::Unit => Err(Error::new(
+            data_struct.struct_token.span,
+            "derive-system does not support derive for unit structs yet",
+        )),
+    }
+}
+
+fn expand_named_struct(struct_name: &Ident, _fields: &Punctuated<Field, Comma>) -> TokenStream2 {
+    quote! {
+        impl lifecycle::Lifecycle for #struct_name {
+            fn start(self) -> Self {
+                let mut s = self;
+
+                s
+            }
+
+            fn stop(self) -> Self {
+                let mut s = self;
+
+                s
+            }
+        }
+    }
+}
