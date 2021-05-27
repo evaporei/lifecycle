099ebf80294e610ecf8894255101fbe58bca2484
Otávio Pace <otaviopp8@gmail.com>
2021-05-26 23:57:47 -0300

    feat: add simple impl Lifecycle for type received

--- derive-system/src/lib.rs
+++ derive-system/src/lib.rs
@@ -4,9 +4,25 @@ use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(System)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
 
-    let expanded = quote! {};
+    let type_name = &input.ident;
+
+    let expanded = quote! {
+        impl lifecycle::Lifecycle for #type_name {
+            fn start(self) -> Self {
+                let mut s = self;
+
+                s
+            }
+
+            fn stop(self) -> Self {
+                let mut s = self;
+
+                s
+            }
+        }
+    };
 
     expanded.into()
 }
